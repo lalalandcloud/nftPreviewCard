89afdc0201b530b60ea9c5cf106dbe43476e1f9c
molengeek <saoussan.elk@gmail.com>
2025-05-07 14:12:38 +0200

    carteAll

---
+++ src/components/compo.jsx
@@ -0,0 +1,17 @@
+import './compo.css'
+
+function Card (){
+    return(
+        <>
+            <div className='cardAll'>
+                <div className='img'></div>
+                <div className='titre'></div>
+                <div className='text'></div>
+                <div className='prix'></div>
+                <div className='createur'></div>
+            </div>
+        </>
+    )
+}
+
+export default
