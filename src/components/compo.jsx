5ac76039800ea1d18e22b76e30d2cdecfb177782
molengeek <saoussan.elk@gmail.com>
2025-05-07 14:33:43 +0200

    img

--- src/components/compo.jsx
+++ src/components/compo.jsx
@@ -3,15 +3,18 @@ import './compo.css'
 function Card (){
     return(
         <>
-            <div className='cardAll'>
-                <div className='img'></div>
-                <div className='titre'></div>
-                <div className='text'></div>
-                <div className='prix'></div>
-                <div className='createur'></div>
+            <div className='back'>
+                <div className='cardAll'>
+                    <div className='img'></div>
+                    <div className='titre'></div>
+                    <div className='text'></div>
+                    <div className='prix'></div>
+                    <div className='createur'></div>
+                </div>
+
             </div>
         </>
     )
 }
 
-export default
+export default Card
